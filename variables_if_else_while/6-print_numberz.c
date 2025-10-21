7d243af140ba45bdd65c4850026976fc7545ed33
jhinlang <julienhinlang@gmail.com>
2025-10-21 17:25:04 +0000

    feat: create a program to list all digit to 9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- create a program to list all digit to 9
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+int i;
+i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
