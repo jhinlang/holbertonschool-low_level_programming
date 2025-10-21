dbfb408baad3dffdc74068ac7d167a561df9391c
jhinlang <julienhinlang@gmail.com>
2025-10-21 09:36:17 +0000

    feat: print whether the number stored in the variable n is positive or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * return: Always (0) success
+ *
+ * making a programm with if else conditions
+ */
+int main(void)
+{
+if (n > 0)
+{
+printf("is positive \n");
+}
+else if (n == 0)
+{
+printf("is zero \n");
+}
+else
+{
+printf("is negative \n");
+}
+return (0);
+}
