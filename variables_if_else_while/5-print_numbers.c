6260ab2f8dae2cbbd826cc814b78e17363268a3d
jhinlang <julienhinlang@gmail.com>
2025-10-21 17:04:59 +0000

    "fix: correcting errors to answer betty 's rules"

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - list all single digits of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+putchar(i + "0");
+i++;
+}
+putchar('\n');
+return (0);
+}
