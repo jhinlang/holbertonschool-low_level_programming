f552ae4438b631cd355f5493ab76689153f658e6
jhinlang <julienhinlang@gmail.com>
2025-10-22 11:26:45 +0000

    "feat: create a program thats prints putchar"

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - create a programm that prints _putchar
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+putchar("_putchar");
+putchar('\n');
+return (0);
+}
