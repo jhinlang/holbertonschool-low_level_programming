2b59a85f21b94d8c8d70eedf8ce6aba1e49adade
jhinlang <julienhinlang@gmail.com>
2025-10-22 14:04:43 +0000

    feat: create a function tthat prints alphabet in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main- Write a function that prints the alphabet, in lowercase.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int i;
+i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
