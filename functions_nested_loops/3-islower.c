b97912774d8d88c88120c9cd589615d4c4d1aabf
jhinlang <julienhinlang@gmail.com>
2025-10-23 10:54:11 +0000

    fix : changing syntax to answer betty's rules

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _islower - entry point for check lowercase character
+*
+* Return: Always 0 (Success)
+*
+*/
+int _islower(int c)
+{
+int c;
+if (c >= 61 && c <= 122)
+	{
+	_putchar('1');
+else
+	_putchar('0');
+	}
+	_putchar('\n');
+	return (0);
+}
