2697685554df7ce8d98a9ae5cc3cab183a429a73
Julien <julien.hinlang@holbertonstudents.com>
2025-10-23 16:08:19 +0200

    "feat: write prototype in main.h and use it to computes absolute value of character"

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - entry point to computes the absolutes value of integer
+ * @n: is the character to check
+ *
+ * Return: the absolute value of n character
+ *
+ */
+int _abs(int n)
+{
+	if (n <0)
+		return (-n);
+	else
+		return (n);
+}	
