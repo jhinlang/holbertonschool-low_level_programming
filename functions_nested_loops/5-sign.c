218662df5165eb80263523dcb6c612d9c68a98dc
jhinlang <julienhinlang@gmail.com>
2025-10-23 12:31:48 +0000

    "fix: change lexic to answer Betty's rules"

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
- * print_sign _ entry point for check the sign of a number
- * @c: the character to check
+ * print_sign - entry point for check the sign of a number
+ * @n: the character to check
  * Return: 1 + if is positive, 0, or -1 if negative
  *
  */
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
 		_putchar('+');
+	}
 	else if (n < 0)
+	{
 		_putchar('-');
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
-		
