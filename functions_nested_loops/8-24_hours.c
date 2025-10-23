91fb09d87c1b70fb365a1fa9611f9dd3f2deeacf
Julien <julien.hinlang@holbertonstudents.com>
2025-10-23 16:54:04 +0200

    feat: adding a prototype to main.h and use it to prints every minutes of a day"

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point to prints every minute of the day
+ *
+ * Return: every minutes of 24H
+ *
+ */
+void jack_hauer(void)
+{
+    int hour, minute;
+
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minute = 0; minute < 60; minute++)
+        {
+            _putchar((hour / 10) + '0');   
+            _putchar((hour % 10) + '0');
+            _putchar(':');
+            _putchar((minute / 10) + '0');
+            _putchar((minute % 10) + '0');
+            _putchar('\n');
+        }
+    }
+}
