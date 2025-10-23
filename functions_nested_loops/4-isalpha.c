a09443a842ed6104da28782bbb7e6cf6ed2e1c80
jhinlang <julienhinlang@gmail.com>
2025-10-23 11:30:41 +0000

    "Feat: adding prototype isalpha in main and creating function to know if the variable is a character "

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - entry point for check alphabetic character
+ * @c: thecharacter to check
+ * Return: 1 if uppercase, 0 if lowercase
+ *
+ */
+int _alpha(int c);
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
