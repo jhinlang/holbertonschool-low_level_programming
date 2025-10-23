55772c3cd47180f64f6dc2a0fe9e6fae8fce2143
jhinlang <julienhinlang@gmail.com>
2025-10-23 11:52:21 +0000

    fix: creating a function to print number sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+ * print_sign _ entry point for check the sign of a number
+ * @c: the character to check
+ * Return: 1 + if is positive, 0, or -1 if negative
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+		_putchar('+');
+	else if (n < 0)
+		_putchar('-');
+		return (1);
+	else
+		return (0);
+}
+		
