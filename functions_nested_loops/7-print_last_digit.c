b895c9e7f142ed3aa7891ded33a101118fd4b718
Julien <julien.hinlang@holbertonstudents.com>
2025-10-23 16:24:14 +0200

    feat: adding prototype in main.h and use it to print last digit of a number"

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point to prints the last digit of a number
+ *
+ * Return: the last digit
+ *
+ */
+int print_last_digit(int n)
+{
+	int i;
+	n = i % 10;
+	_putchar(n);
+	return (0);
+}
+
