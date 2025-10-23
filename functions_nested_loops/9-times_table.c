47bf2c07b50bdbae14726d18852e0fa322d9c9cd
Julien <julien.hinlang@holbertonstudents.com>
2025-10-23 17:30:29 +0200

    "feat: creating a prototype and use it to prints the 9 tables"

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - entry point to prints the 9 times table
+ *
+ * Return: the numbers asked
+ *
+ */
+void times_table(void)
+{
+int i;
+
+while ( i < 10)
+{
+	i * 0;
+	_putchar(i);
+	_putchar(',');
+	_putchar(' ');
+	i++;
+}	
+
