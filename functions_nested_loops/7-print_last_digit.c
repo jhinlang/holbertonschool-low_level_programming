0d88c64c7d5a16ab711a7b44f9086cab3f5877a6
Julien <julien.hinlang@holbertonstudents.com>
2025-10-23 16:35:03 +0200

    "fix: correcting lexiq to answer betty's rules"

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int i;
-	n = i % 10;
-	_putchar(n);
-	return (0);
+	int last_digit;
+	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit = -last_digit;
+	
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
 
