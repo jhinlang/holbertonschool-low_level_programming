5cb224811298ed38055850f1e3fc4d53291af1f2
Julien <julien.hinlang@holbertonstudents.com>
2025-10-24 13:54:57 +0200

    "fix: changing syntax to answer betty's style"

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,9 +3,9 @@
  * add - entry point to adds two integers and return result
  * @a: character to add with b
  * @b: character to addd with a
- * 
+ *
  * Return: the summ of the two integers
- * 
+ *
  */
 int add(int a, int b)
 {
