7954229badffc80f79f6f87bd102b50d6a542ffe
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 11:23:31 +0100

    feat: creating a function to draw a line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - entry point to print a line in the terminal
+ * @n: the number of times the line should be print
+ *
+ * Return: always __
+ *
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		while ( i <= n)
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+	}
+}
