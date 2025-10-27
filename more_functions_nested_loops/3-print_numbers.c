ee62c18f3d161723bc7e5cc0c308721a5d62c295
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 09:44:12 +0100

    creating a function that prints number from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - entry point to display numbers from 0 to 9 
+ *
+ * return : always 0 
+ *
+ */
+void print_numbers(void);
+{
+int i = 0;
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar("\n");
+}
