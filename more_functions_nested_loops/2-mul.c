6536697f2a4f7af1e815d71d1ba0c452ce9b40e7
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 09:36:59 +0100

    feat: creating a function to multiplicate two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - is entry point to multiplication a by b
+ * @a: is a character to multiply
+ * @b: is a character to multiply
+ *
+ * Return: the result of the multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
