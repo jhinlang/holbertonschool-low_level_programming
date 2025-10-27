3b8bcca14ecdf65f10ff8b35d881516d892ca542
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 15:33:00 +0100

    creating a function to prints numbers from 1 to 100

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * fizz_buzz - entry point
+ *
+ * Return: numbers and fizz buzz
+ *
+ */
+int main(void)
+{
+	for ( i = 1; i < 100; i++)
+	{
+		if ( i % 3 = 0)
+		printf('Fizz ');
+		else if (i % 5 = 0)
+			printf('Buzz ')
+		else if (i % 3 = 0 && i % = 0)
+			printf('FizzBuzz ')
+		else
+			printf(i);
+	
