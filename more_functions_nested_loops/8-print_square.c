99fb6b74ac216e9fc38293d56be6b9621904baff
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 14:52:33 +0100

    fix: correcting mistakes lexic

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,17 +9,16 @@ void print_square(int size)
 {	
 	int j;
 	int i;
-	int size = 0;
 	if (size <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
 	for (i = 0; i < size; i++)
 	{
 		for(j = 0; j < size; j++)
-		{
 			_putchar('#');
-			_putchar('\n');
-		}
+		_putchar('\n');
+
 	}
 }
