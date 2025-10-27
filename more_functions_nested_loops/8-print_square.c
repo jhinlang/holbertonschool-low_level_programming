716581892f24c3f135b19d21aca95b1c7a7a25aa
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 14:54:49 +0100

    fix: correcting some errors to answer betty's rules

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,17 +6,18 @@
  *
  */
 void print_square(int size)
-{	
+{
 	int j;
 	int i;
-	if (size <= 0)
+	if 
+	(size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 			_putchar('#');
 		_putchar('\n');
 
