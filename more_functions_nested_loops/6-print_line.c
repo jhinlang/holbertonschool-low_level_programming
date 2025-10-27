49baaaa7512dd82c2c1b2343ef98c45959aab77f
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 11:43:36 +0100

    correcting lexic to answer betty style

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,18 @@
  */
 void print_line(int n)
 {
-	int i = 0;
+int i = 0;
 if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (i < n)
-		{
-			_putchar('_');
-			i++;
-		}
-		_putchar('\n');
-	}
+{
+_putchar('\n');
+}
+else
+{
+while (i < n)
+{
+_putchar('_');
+i++;
+}
+_putchar('\n');
+}
 }
