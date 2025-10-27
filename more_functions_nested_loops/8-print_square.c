6d997f35191edf28c50167a776484ddf89a1734f
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 14:45:33 +0100

    feat: creating a function to draw a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - entry point to draw a square
+ *
+ * Return : nothing
+ *
+ */
+void print_square(int size)
+{	
+	int j;
+	int i;
+	int size = 0;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for(j = 0; j < size; j++)
+		{
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
