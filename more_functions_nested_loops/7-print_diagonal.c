37ddfcbefe4be4ff58edd39f8aab4e903bc8027d
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 14:21:44 +0100

    fix: correcting syntax to answer betty's rules

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,24 +3,23 @@
  * print_diagonal - entry point to draw a diagonal line on the terminal
  * @n: is the number of times the character \ shoud appear
  *
- * Return: nothing 
+ * Return: nothing
  *
  */
 void print_diagonal(int n)
 {
-	int i;
-	int j;
-	if ( n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
-
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < i; j++)
-            _putchar(' ');
-        _putchar('\\');
-        _putchar('\n');
-    }
+int i;
+int j;
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
 }
