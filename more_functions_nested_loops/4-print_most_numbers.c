9d0388e6f120d470c1360b162c117b4d3289c491
Julien <julien.hinlang@holbertonstudents.com>
2025-10-27 10:21:27 +0100

    fix: corrzcting lexic to answer betty style

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - entry point to print all numbers from 0 to 9 except 2 and 4
+ * print_most_numbers - entry point to print all numbers 
  *
  * Return: the numbers
  *
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 int i = 0;
 	while (i <= 9)
 	{
-		if (i != 2 && i!=4)
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
-		}	
+		}
 		i++;
-	}		
+	}
 	_putchar('\n');
-}	
+}
 
