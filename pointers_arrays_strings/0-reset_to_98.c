cbc5736e8e4d7b4d68ce91e33f22d47056d470c4
Julien <julien.hinlang@holbertonstudents.com>
2025-10-28 14:21:10 +0100

    fix: creating a function to updates value of pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - entry point to updates the value
+ *
+ * Return: the value updated
+ *
+ */
+void reset_to_98(int *n)
+{
+	int *n;
+	*n = &n;
+	*n = 98;
+}
