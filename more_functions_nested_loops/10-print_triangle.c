abc2556aaf97e282a5cb8f62f6f85a71864cf38f
Julien <julien.hinlang@holbertonstudents.com>
2025-10-28 01:50:32 +0100

    feat: creation of a function to draw a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - entry point to draw a triangle
+ * @size: is the dimensions
+ *
+ * Return: a draw of a triangle 
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j < size; j++)
+			_putchar(' ');
+
+		for (j = 1; j <= i; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
