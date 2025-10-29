39e813decc84dfdf81cef201f1bc125138613a8a
Julien <julien.hinlang@holbertonstudents.com>
2025-10-29 14:41:42 +0100

    feat: ajouter la fonction puts_half pour imprimer la seconde moitié d'une chaîne

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - print the second half of the string
+ * @str: is the variable to print the half
+ *
+ * Return: the half of the string
+ */
+void puts_half(char *str);
+{
+	int i, j, k;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	if (j % 2 == 0)
+	{
+		k = j /2;
+		for (i = k; i < j; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
