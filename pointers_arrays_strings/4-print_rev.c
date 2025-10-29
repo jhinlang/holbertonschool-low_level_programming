d432a63ac24d97870ae76acfc526d6d185197424
Julien <julien.hinlang@holbertonstudents.com>
2025-10-29 10:49:21 +0100

    fix: adding a function _strlen

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,22 @@
  *
  * Return: a string in reverse
  */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+
+return (len);
+}
 void print_rev(char *s)
 {
 	int l = 0;
-	int r = strlen(s) -1;
+	int r = _strlen(s) -1;
 	char t;
 
 	while (l < r)
