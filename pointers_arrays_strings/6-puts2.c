a6d95df9edbaded1651466b470ff29637c18ebe1
Julien <julien.hinlang@holbertonstudents.com>
2025-10-29 14:16:27 +0100

    feat: ajouter la fonction puts2 pour imprimer chaque caractère d'une chaîne

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - entry point to prints every character of a string
+ *
+ * @str:variables to compare and prints if are ok
+ *
+ * Return: str which are good
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != "\0")
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i];
+			i++;
+	}
+		_putchar('\n');
+}
