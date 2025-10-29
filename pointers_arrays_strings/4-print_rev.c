7459c1586d2c9236518a3e75630312bcfcb52930
Julien <julien.hinlang@holbertonstudents.com>
2025-10-29 10:37:25 +0100

    feat: creating a prototype and a function to print a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - main entry to prints a string in reverse
+ * @s: character to check
+ *
+ * Return: a string in reverse
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int r = strlen(s) -1;
+	char t;
+
+	while (l < r)
+	{
+		t = s[l];
+		s[l] = s[r];
+		s[r] = t;
+
+		l++;
+		r--;
+	}
+}
