c285d265f4d031f5a2e5a98bc1061e42a6ccfd29
Julien <julien.hinlang@holbertonstudents.com>
2025-10-29 15:48:32 +0100

    fix: changin indent, and spaces to answer betty's rules

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,15 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
 
-        if (i != n - 1)
-            printf(", ");
-    }
-
-    printf("\n");
+		if (i != n - 1)
+			print(", ");
+	}
+	printf("\n#);
 }
 
