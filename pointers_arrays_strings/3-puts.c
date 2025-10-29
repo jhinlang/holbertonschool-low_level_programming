8db3fef6b057dc29b96ef76cea18d7f6ffead04f
Julien <julien.hinlang@holbertonstudents.com>
2025-10-29 09:09:05 +0100

    adding prototype to prints a str and the function

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - entry point to prints a string followed by a new line, to stdout
+ * @*str: is a pointer
+ *
+ * Return: the string followed by a new line
+ *
+ */
+void _puts(char *str)
+{
+	*str = &str;
+		str = " I do not fear computers. I fear the lack of them - Isaac Asimov";
+}
