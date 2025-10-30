aaecdaec107c5e179835486ae2fee611c5235fa4
Julien <julien.hinlang@holbertonstudents.com>
2025-10-30 14:24:28 +0100

    fix: logic  error, infinite loop

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,15 +10,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	char *save = dest;
 
-	while (src[i != '\0'])
-	{
-		dest[i] = src[i];
-		i++;
+	while ((*dest++ = *src++) !='\0'=)
+	;
 
-	}
-	dest[i] = '\0';
-
-	return dest;
+	return save;
 }
