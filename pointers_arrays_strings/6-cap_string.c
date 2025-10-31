c88c389a03b65ef199ea8913b26330913baf3284
Julien <julien.hinlang@holbertonstudents.com>
2025-10-31 15:51:32 +0100

    feat: creating a function to capitalize all words of string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - put the first letter of each word in upper
+ * @s: string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '!', '?', '"', '(', ')', '{', '}', };
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 &&(s[i] >='a' && s[i] <= 'z'))
+			s[i] -= 32;
+		else
+		{
+			for (j = 0; j < 13; j++)
+			{
+				if (s[i - &] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				{
+						s[i] -= 32;
+						break;
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
+
