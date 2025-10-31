66a8fbecf011927c523a0859648675ef7c6401eb
Julien <julien.hinlang@holbertonstudents.com>
2025-10-31 11:42:35 +0100

    feat: creating a function to concatene 2 strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncat - entry point to concatene 2 strings in 1
+ * @dest: is the destination
+ * @src: is the source
+ * @n: is the number of variables 
+ *
+ * Return: dest
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
