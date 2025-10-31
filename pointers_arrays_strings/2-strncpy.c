d3011d0a7eaf5f897e6510a3350072838aecc830
Julien <julien.hinlang@holbertonstudents.com>
2025-10-31 14:52:48 +0100

    fix : correcting syntax to answer's betty rules

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,18 +12,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 
-	while (src[j] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
-		dest[i] = src[j];
-			j++;
-			i++;
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 
-	while (n > j)
-	{
-		printf('\0');
-	}
 	return (dest);
 }
