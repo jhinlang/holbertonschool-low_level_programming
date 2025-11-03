d33c6189f263cb54ce12072108d4ae36b38e7416
Julien <julien.hinlang@holbertonstudents.com>
2025-11-03 14:55:39 +0100

    fix: correcting syntax to answer betty's style

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		while(haystack[i + j] == needle[j] && needle[j] != '\0')
+		while (haystack[i + j] == needle[j] && needle[j] != '\0')
 			j++;
 		if (needle[j] == '\0')
 			return (haystack + i);
