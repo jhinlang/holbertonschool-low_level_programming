ab11617eeaa531bf1bd198661bbcc4b6b08e3399
Julien <julien.hinlang@holbertonstudents.com>
2025-11-03 10:49:50 +0100

    feat: creating a function to print the number of common bytes between s and accept

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - entry point to returns bytes of s only if they are in accept too
+ * @s: is the characters to check with accept
+ * @accept: is the characters to check with s
+ *
+ * Return: numbers of common characters 
+ *
+ */
+unsigned int _strspn(char*s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0' && s[i] != accept[j]; j++)
+			;
+
+		if (accept[j] =='\0')
+			break;
+	}
+	return (i);
+}
