2035583b75eca5aed04c79d67782f4c6f5b0138e
Julien <julien.hinlang@holbertonstudents.com>
2025-11-03 09:59:31 +0100

    fix: correcting error to compile bad return char

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,21 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
-			i++;
+		i++;
 		}
-		else
-			i++;
+		if (s[i] == c)
+		{
+			return (s + i);
+
+		}
+		i++;
 	}
 	return (NULL);
 }
