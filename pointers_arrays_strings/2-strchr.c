0e1f77aab65c135da8bf7d5df0c411629e699a5d
Julien <julien.hinlang@holbertonstudents.com>
2025-11-03 09:31:42 +0100

    feat: creating a function to seach a character

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - entry point to search a character
+ * @s: is the character in array to compare with c
+ * @c: is the character to find
+ *
+ * Return : s
+ *
+ */
+char *_strchr(char *s, char c);
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != c; i++)
+	{
+		return (*s);
+	}
+	return (NULL);
+}
