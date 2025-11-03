9a9fb7337672283b55873637c007d5327f26ce46
Julien <julien.hinlang@holbertonstudents.com>
2025-11-03 08:28:47 +0100

    feat: creating a function to copy n bytes

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - entry point to copy an area memory 
+ * @s: is the pointer of destination
+ * @b: is the text to copy
+ * @n: is the bytes to copy
+ *
+ * Return: s
+ *
+ */
+char * _memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
