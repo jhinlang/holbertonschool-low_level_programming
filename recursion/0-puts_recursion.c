089f0916bc8b02f455845327e65cec55c23d15f6
Julien <julien.hinlang@holbertonstudents.com>
2025-11-05 11:18:11 +0100

    feat: create a new directory

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - entry point to prints a string recursively
+ * @s: is the string to print
+ *
+ * Return: the string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+		
+
---
+++ recursion/README.md
@@ -0,0 +1,3 @@
+Hello i'm a student at holberton school, i working this week on C, precisely on recursions and argc, argv.
+
+So Grab popconr, take a seat and enjoy the show ! 
