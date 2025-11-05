02b346a168bfbf6d454a0f020c8f4c788a1119e4
Julien <julien.hinlang@holbertonstudents.com>
2025-11-05 12:06:33 +0100

    feat: create a new function to print lenght of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ * _strlen_recursion - entry point to count the string's length recursively
+ * @s: is the string to count
+ *
+ * Return: the length of string
+ *
+ */
+int _strlen_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
