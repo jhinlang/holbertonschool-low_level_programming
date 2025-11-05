274015e6b07861df175cae42b24efc6c20f26022
Julien <julien.hinlang@holbertonstudents.com>
2025-11-05 15:29:52 +0100

    feat: refactor square root functions for clarity and correctness

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _sqrt_recursion - is the entry point to return the natural square root
+ * _sqrt_help - is the entry point to return the natural square root
  * @n: is the number to return square root
+ * @x: is the check value
  *
  * Return: the square root of n
  *
  */
-int _sqrt_recursion(int n)
+int _sqrt_help(int n, int x)
 {
-	int x = 0;
-
 	if (x * x > n)
 	{
 		return (-1);
@@ -19,5 +18,21 @@ int _sqrt_recursion(int n)
 	{
 		return (x);
 	}
-	return (_sqrt_recursion(n, x + 1));
+	return (_sqrt_help(n, x + 1));
 }
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: number to find the root square
+ * 
+ * Return: natural square root or -1  if no exists
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return(_sqrt_help(n,0));
+}
+
