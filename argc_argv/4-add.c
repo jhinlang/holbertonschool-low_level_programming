d0884abf34a184ea4876c9b9f15fe18e678b79cd
Julien <julien.hinlang@holbertonstudents.com>
2025-11-06 16:07:43 +0100

    feat: creating a function to asumm all arguments

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point to summ all arguments
+ * @argc: is the number of arguments
+ * @argv: is the values of arguments
+ * Return: the sum of arguments or error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
