fa6b823eb18b733df2f6e677e7fb518521484165
Julien <julien.hinlang@holbertonstudents.com>
2025-11-06 14:48:50 +0100

    feat: creating a function to print the name with argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point to print the name
+ *
+ * Return: the name
+ *
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Hello, i'm a student at Holberton school and i'm working on argc and argv today.
