02e542859705d8ef381cd5a23850e67e057da123
Julien <julien.hinlang@holbertonstudents.com>
2025-11-14 12:52:44 +0100

    feat: declaring a struct variable

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - entry point to create a function that give stats about a dog
+ * @d: is the structure
+ * @name: is the dog name
+ * @age: is the dog age
+ * @owner: is the dog owner
+ *
+ * Return: the dog informations
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
