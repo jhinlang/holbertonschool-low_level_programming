e5b77dda34a35c2e7510412a3ed4cd4982a49d1c
Julien <julien.hinlang@holbertonstudents.com>
2025-11-14 15:26:46 +0100

    feat: #freedog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to the dog to free
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
