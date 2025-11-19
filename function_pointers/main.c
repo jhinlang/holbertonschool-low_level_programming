63eea7614b279390da7a2bb434d0f84cf14785a4
Julien <julien.hinlang@holbertonstudents.com>
2025-11-19 14:41:08 +0100

    feat: starting task 3, the calc

---
+++ function_pointers/main.c
@@ -0,0 +1,38 @@
+#include "calc.h"
+/**
+ * main - a programm that performs simple operations
+ * Return: the result
+ * 
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*func)(int, int);
+	
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	func = get_op_func(argv[2]);
+
+	if (func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = func(a, b);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ function_pointers/op_functions.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+/**
+ * op_add - entry point to sum a and b
+ * @a: an int to sum with b
+ * @b: an int to sum with a
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point to returns the difference of a and b
+ * @a: is one of integer in the sub
+ * @b: is one of integer in the sub
+ *
+ * Return: the diferrence of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - is the entry point to returns the product of a and b
+ * @a: is an integer to mul
+ * @b: is an integer to mul
+ *
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point to the result of a divisinof a by b
+ * @a: is the integer to divide
+ * @b: is the integer will divide a
+ *
+ * Return: the result of the devision of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - is the entry point to modulo a by b
+ * @a: is the integer to modulo
+ * @b: is the integer wich will modulo a
+ *
+ * Return: the remainder of the division of a by b
+ *
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
