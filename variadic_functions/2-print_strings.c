17574f39e4c31c62f8f1c2c398e7fc02352b75b6
Julien <julien.hinlang@holbertonstudents.com>
2025-11-21 15:30:32 +0100

    feat: creating a function that prints strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+/**
+ * prints_strings - is entry point to print strings
+ * @separator: is the string to be printed between the strings
+ * @n: is the number of string passed to the function
+ *
+ * Return: the strings printed
+ */
+void print_strings( const char *separator, const unsigned int n, ...)
+{
+	unsigned int i; 
+	char *str;
+
+	va_list list; 
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char*);
+
+		if (str == NULL)
+		{
+			printf("(nill)");
+		}
+		else 
+		{
+			printf("%s", str);
+		}
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+	va_end(list);
+}
