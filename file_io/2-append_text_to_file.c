87c244ca67421a21a6c3ba20d25da6fad0c2f8c0
Julien <julien.hinlang@holbertonstudents.com>
2025-12-05 22:46:06 +0100

    feat:write a function appends text at the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,13 +3,13 @@
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file to append to
  * @text_content: content to append to the file
- * 
+ *
  * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 
