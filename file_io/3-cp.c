224f8ac03aa97f9c7b8d01cd6e5716e29eec7f72
Julien <julien.hinlang@holbertonstudents.com>
2025-12-05 22:58:12 +0100

    feat: writing a program that copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,75 @@
+#include "main.h"
+/**
+ * close_fd -closes file descriptors and handles errors
+ * @fd: file descriptor to close
+ */
+void close_fd(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, exit with code on failure
+ */
+int main(int argc, char **argv[])
+{
+	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close_fd(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+		{
+			close_fd(fd_from);
+			close_fd(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		close_fd(fd_from);
+		close_fd(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
